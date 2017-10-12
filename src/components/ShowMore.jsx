69e9d7ded49a60d4356951e6713c4926b8e4bed8
Stephane Moroso <stephane.moroso@gmail.com>
2017-10-12 23:09:14 +0200

    Simple state use demo

---
+++ src/components/ShowMore.jsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+class ShowMore extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      limit: props.limit
+    }
+
+    this.showMore = this.showMore.bind(this);
+  };
+
+  showMore() {
+    //option1
+    //const newLimit = this.state.limit + 5;
+    //this.setState({limit: newLimit});
+
+    //option2
+    this.setState((prevState, props) => {
+      return { limit: prevState.limit + 5 };
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.showMore}>Show More</button><br/>
+        <span>Limit: {this.state.limit}</span>
+      </div>
+    );
+  }
+}
+
+ShowMore.propTypes = {
+  limit: React.PropTypes.number
+}
+
+ShowMore.defaultProps = {
+  limit: 10
+}
+
+export default ShowMore;
